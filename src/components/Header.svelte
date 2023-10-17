ad699846c1bb924e311a87ba98d250532ba32b30
Matheus <matriblima97@gmail.com>
2023-10-17 19:51:04 +0200

    cambios aplicados

---
+++ src/components/Header.svelte
@@ -0,0 +1,17 @@
+<nav>
+    <div>
+        <img src="" alt="">
+    </div>
+    <ul>
+        <li>Inicio</li>
+        <li>Peliculas</li>
+        <li>Series</li>
+        <li>Colecciones</li>
+        <li>Festivales</li>
+        <li>Más</li>
+    </ul>
+  </nav>
+
+  <style>
+    
+  </style>
